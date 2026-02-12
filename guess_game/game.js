215e4832a046db7932aacefd2abfb8f4e62d38a1
306 - Diseño web <306 - Diseo web>
2026-02-12 14:41:40 -0500

    Respuestas y resultados

--- guess_game/game.js
+++ guess_game/game.js
@@ -1,9 +1,20 @@
 const number = Math.floor(Math.random() * 100) + 1;
-let intento = 0;
-let adivinado = false;
+let intent = 0;
+let guessed = false;
 const readline =require("readline").createInterface({
     input: process.stdin,
     output: process.stdout
 });
-console.log("adivina el numero de 1 a 100")
+console.log("adivina el numero de 1 a 100");
+function preguntar() {
+    readline.question("ingresa un numero: "), entrada => {
+    const numero = parseInt(entrada);
+    intento++;
+    if (isNaN(numero) || numero < 1 || numero > 100){
+        console.log("ingrese un numero entre 1 y 100");
+    }else if (numero === number) {
+        console.log("Wooow adivinaaste el numero ${number} en ${intent}")
+    }
+    }
+}
 
