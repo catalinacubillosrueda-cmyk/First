5d16478eebd7a49ee3d9d92c3acaae86a2c8df8c
306 - Diseño web <306 - Diseo web>
2026-02-12 14:27:23 -0500

    game.js uploaded

--- guess_game/game.js
+++ guess_game/game.js
@@ -5,5 +5,5 @@ const readline =require("readline").createInterface({
     input: process.stdin,
     output: process.stdout
 });
-console.log("adivina el numero de 1 a 100");
+console.log("adivina el numero de 1 a 100")
 
