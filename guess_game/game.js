00f3bc19e53c362337438ac429bf494d3dc05a5b
306 - Diseño web <306 - Diseo web>
2026-02-12 14:26:46 -0500

    First commit:game.js uploaded

---
+++ guess_game/game.js
@@ -0,0 +1,9 @@
+const number = Math.floor(Math.random() * 100) + 1;
+let intento = 0;
+let adivinado = false;
+const readline =require("readline").createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+console.log("adivina el numero de 1 a 100");
+
